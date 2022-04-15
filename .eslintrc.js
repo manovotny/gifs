23cb12cc54c615ad9c715800039a5cbf841fc052
Michael Novotny <manovotny@gmail.com>
2022-04-15 15:06:36 -0500

    Fixes remaining issues.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,10 @@
-const fixedInNextVersionOfGOML = {
-    '@typescript-eslint/no-misused-promises': 0,
-    'jsx-a11y/anchor-is-valid': 0,
-};
-
 module.exports = {
     extends: 'get-off-my-lawn',
     ignorePatterns: ['dist', 'main', 'renderer/.next', 'renderer/out'],
     rules: {
-        ...fixedInNextVersionOfGOML,
-        '@typescript-eslint/no-unnecessary-condition': 0,
-        '@typescript-eslint/no-unsafe-call': 0,
-        '@typescript-eslint/no-unsafe-member-access': 0,
+        // All will be fixed in the next version of GOML.
+        '@typescript-eslint/no-misused-promises': 0,
         '@typescript-eslint/unbound-method': 0,
+        'jsx-a11y/anchor-is-valid': 0,
     },
 };
