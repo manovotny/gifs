cbde1722d35fb965ca4bfb4a7c794e8ba4616d54
Michael Novotny <manovotny@gmail.com>
2022-04-20 16:18:55 -0500

    Sigh...

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
     extends: 'get-off-my-lawn',
     ignorePatterns: ['dist', 'main', 'renderer/.next', 'renderer/out'],
     rules: {
+        // Sigh...
         '@typescript-eslint/no-unnecessary-condition': 0,
+        '@typescript-eslint/no-unsafe-call': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
     },
 };
