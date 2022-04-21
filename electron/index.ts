1f25422cf254fc3d71bd7ff89aa1e06174ea04d7
Michael Novotny <manovotny@gmail.com>
2022-04-21 17:05:07 -0500

    Removes deprecated `format` usage.

--- electron/index.ts
+++ electron/index.ts
@@ -1,12 +1,10 @@
 import {join} from 'path';
-import {format} from 'url';
 import type {IpcMainEvent} from 'electron';
 import {BrowserWindow, app, ipcMain} from 'electron';
 
 import isDev from 'electron-is-dev';
 import prepareNext from 'electron-next';
 
-// Prepare the renderer once the app is ready
 app.on('ready', async (): Promise<void> => {
     await prepareNext('./renderer');
 
@@ -20,21 +18,15 @@ app.on('ready', async (): Promise<void> => {
         width: 800,
     });
 
-    const url = isDev
-        ? 'http://localhost:8000/'
-        : format({
-              pathname: join(__dirname, '../renderer/out/index.html'),
-              protocol: 'file:',
-              slashes: true,
-          });
+    const url = isDev ? 'http://localhost:8000/' : join('file://', __dirname, '../renderer/out/index.html');
 
     await mainWindow.loadURL(url);
 });
 
-// Quit the app once all windows are closed
+// Quit the app once all windows are closed.
 app.on('window-all-closed', app.quit);
 
-// listen the channel `message` and resend the received message to the renderer process
+// Listen the channel `message` and resend the received message to the renderer process.
 ipcMain.on('message', (event: IpcMainEvent, message: string) => {
     // eslint-disable-next-line no-console
     console.log(message);
--- electron/preload.ts
+++ electron/preload.ts
@@ -7,8 +7,8 @@ declare global {
     var ipcRenderer: IpcRenderer;
 }
 
-// Since we disabled nodeIntegration we can reintroduce
-// needed node functionality here
+// Since we disabled `nodeIntegration` we can reintroduce
+// needed node functionality here.
 process.once('loaded', () => {
     global.ipcRenderer = ipcRenderer;
 });
