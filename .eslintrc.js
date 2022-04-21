dbf708396bd4ae1979e63ed4f177c5336b572efb
Michael Novotny <manovotny@gmail.com>
2022-04-21 17:01:05 -0500

    Removes unnecessary eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,4 @@
 module.exports = {
     extends: 'get-off-my-lawn',
     ignorePatterns: ['dist', 'main', 'renderer/.next', 'renderer/out'],
-    rules: {
-        // Sigh...
-        '@typescript-eslint/no-unnecessary-condition': 0,
-        '@typescript-eslint/no-unsafe-call': 0,
-        '@typescript-eslint/no-unsafe-member-access': 0,
-    },
 };
